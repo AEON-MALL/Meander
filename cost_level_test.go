39b0e43a932802fd481279ffb8c6b3a96a693302
AEON-MALL <jbytq@yahoo.co.jp>
2022-05-22 23:32:40 +0900

    add test

---
+++ cost_level_test.go
@@ -0,0 +1,38 @@
+package meander_test
+
+import (
+	"meander/meander"
+	"testing"
+
+	"github.com/cheekybits/is"
+)
+func TestCostValues(t *testing.T){
+	is := is.New(t)
+	is.Equal(int(meander.Cost1),1)
+	is.Equal(int(meander.Cost2),2)
+	is.Equal(int(meander.Cost3),3)
+	is.Equal(int(meander.Cost4),4)
+	is.Equal(int(meander.Cost5),5)
+}
+
+func TestCostString(t *testing.T){
+	is := is.New(t)
+	is.Equal(meander.Cost1.String(), "$")
+	is.Equal(meander.Cost2.String(), "$$")
+	is.Equal(meander.Cost3.String(), "$$$")
+	is.Equal(meander.Cost4.String(), "$$$$")
+	is.Equal(meander.Cost5.String(), "$$$$$")
+}
+
+func TestParseCost(t *testing.T){
+	is := is.New(t)
+	is.Equal(meander.Cost1, meander.ParceCost("$"))
+	is.Equal(meander.Cost2, meander.ParceCost("$$"))
+	is.Equal(meander.Cost3, meander.ParceCost("$$$"))
+	is.Equal(meander.Cost4, meander.ParceCost("$$$$"))
+	is.Equal(meander.Cost5, meander.ParceCost("$$$$$"))
+}
+
+func TestParseCostRange(t *testing.T){
+	
+}
